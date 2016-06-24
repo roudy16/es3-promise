00ffaa54c5b0530755f2450e8f3ee613a922fb75
marzio <marzio@debian-mac.home>
2016-06-24 21:57:28 +0100

    UPDATE: instead of using an internal map of promises, use an enhancer on each promise

--- src/promise.js
+++ src/promise.js
@@ -4,8 +4,6 @@ module.exports = (function(global) {
 	var FULFILLED = 'fulfilled';
 	var REJECTED = 'rejected';
 
-	var id = 0;
-
 	function emptyFn() {}
 	function isFn(fn) { return fn && typeof fn === 'function'; }
 
@@ -13,39 +11,182 @@ module.exports = (function(global) {
 	function isFulfilled(promise) { return promise.state === FULFILLED; }
 	function isRejected(promise) { return promise.state === REJECTED; }
 
-	function resolveFn(promise) {
+	function clean(enhancedPromise, enhancedState) {
+
+		var promise = enhancedPromise.promise;
+		// if the promise is not pending
+		// then it's either resolved or rejected
+		if (enhancedState.isStateFn(promise)) {
+
+			// get the then stack based on the promise state
+			var stack = enhancedPromise['on' + enhancedState.state];
+
+			// while there are elements in the stack
+			while (stack.length) {
+
+				// 2.2.2: If `onFulfilled` is a function
+				// 2.2.2.3: it must not be called more than once.
+				// remove the first element of the stack
+				// then calls are handled on registering order
+				var element = stack.shift();
+
+				// the nextEnhancedPromise returned by the specific then
+				var nextEnhancedPromise = element.nextEnhancedPromise;
+				// the then callback (onFulfillment or onRejection)
+				var callback = element.callback;
+				// the data of the promise (value or reason)
+				var data = promise[ enhancedState.dataName ];
+
+				// 2.2.1: Both `onFulfilled` and `onRejected` are optional 
+				// arguments.
+				// 2.2.1.1: If `onFulfilled` is not a function, it must be 
+				// ignored.
+				// 2.2.1.2: If `onRejected` is not a function, it must be 
+				// ignored.
+
+				if (isFn(callback)) {
+
+					
+
+					// 2.2.4 + 2.2.2.3: it must not be called more than once.
+					// when multiple `then` calls are made, spaced apart in time
+
+					// when doing the timeout, ensure that the callback 
+					// is the one that is invoked before the timeout
+					// pass the callback as argument of an immediately invoked
+					// function call so that it is not modified by subsequent 
+					// callbacks
+					(function(callback) {
+
+						// 2.2.4: `onFulfilled` or `onRejected` must not be 
+						// called until the execution context stack contains 
+						// only platform code.
+						setTimeout(function() {
+
+						// 2.2.2: If `onFulfilled` is a function
+						// 2.2.2.1: it must be called after `promise` is 
+						// fulfilled, with `promise`’s fulfillment value 
+						// as its first argument.
+        				// 2.2.2.2: it must not be called before `promise` 
+        				// is fulfilled
+						// 2.2.3: If `onRejected` is a function,
+						// 2.2.3.1: it must be called after `promise` is 
+						// rejected, with `promise`’s rejection reason as 
+						// its first argument.
+        				// 2.2.3.2: it must not be called before `promise` 
+        				// is rejected
+        				// 2.2.3.3: it must not be called more than once.
+						// be careful here, callback can be asynchronous
+
+						// 2.2.5 `onFulfilled` and `onRejected` must be 
+						// called as functions (i.e. with no `this` value).
+						// -> thus the apply with undefined as context
+
+						// 2.2.6.1 : multiple fulfillment handlers, one of which throws
+						// need a try catch block
+						try {
+							callback.apply(undefined, [ data ]);
+						} catch(err) {}
+						
+						}, 0);
+
+					})(callback);
+
+				} else {
+					// resolve / reject the nextEnhancedPromise
+					// with the value / reason of the invoking promise
+					enhancedState.execFn(nextEnhancedPromise)(data);
+				}
+
+			}
+			// clear the other stack
+			enhancedPromise['on' + enhancedState.otherState] = [];
+
+		}
+
+	}
+
+	// 2.2.6: `then` may be called multiple times on the same promise.
+	function registerThen(enhancedPromise, nextEnhancedPromise, onFulfillment, onRejection) {
+
+		var promise = enhancedPromise.promise;
+
+		// update the promise property in the map - its state may have 
+		// changed
+		//enhancedPromise.promise = promise;
+
+
+		// 2.2.6.1: If/when `promise` is fulfilled, all respective `onFulfilled` 
+		// callbacks must execute in the order of their originating calls to `then`
+
+		// register the onFulfillment callback in its stack, with the 
+		// nextEnhancedPromise by the then function
+		enhancedPromise.onfulfilled.push({
+			nextEnhancedPromise: nextEnhancedPromise,
+			callback: onFulfillment
+		});
+
+
+		// 2.2.6.2: If/when `promise` is rejected, all respective `onRejected` 
+		// callbacks must execute in the order of their originating calls to `then`
+
+		// register the onRejection callback in its stack, with the 
+		// nextEnhancedPromise by the then function
+		enhancedPromise.onrejected.push({
+			nextEnhancedPromise: nextEnhancedPromise,
+			callback: onRejection
+		});
+
+		// if the promise is already fulfilled
+		if (isFulfilled(promise)) {
+			// execute all the onFulfilled callbacks register within the
+			// various then calls
+			clean(enhancedPromise, enhancedState[FULFILLED]);
+
+		} else if (isRejected(promise)) {
+			// execute all the onRejected callbacks register within the
+			// various then calls	
+			clean(enhancedPromise, enhancedState[REJECTED]);
+
+		}
+
+	}
+
+	function resolveFn(enhancedPromise) {
+		var promise = enhancedPromise.promise;
 		return function (value) {
 			// 2.1.2.1: When fulfilled, a promise: must not transition to any 
 			// other state.
 			if (isPending(promise)) {
 				promise.state = FULFILLED;
 				promise.value = value;
-				cache.clean(promise, enhanced[FULFILLED]);
+				clean(enhancedPromise, enhancedState[FULFILLED]);
 			}
 		};
 	}
 
-	function rejectFn(promise) {
+	function rejectFn(enhancedPromise) {
+		var promise = enhancedPromise.promise;
 		return function (reason) {
 			// 2.1.3.1: When rejected, a promise: must not transition to any 
 			// other state.
 			if (isPending(promise)) {
 				promise.state = REJECTED;
 				promise.reason = reason;
-				cache.clean(promise, enhanced[REJECTED]);
+				clean(enhancedPromise, enhancedState[REJECTED]);
 			}
 		};
 	}
 
-	var enhanced = {};
-	enhanced[FULFILLED] = {
+	var enhancedState = {};
+	enhancedState[FULFILLED] = {
 		state: FULFILLED,
 		otherState: REJECTED,
 		isStateFn: isFulfilled,
 		execFn: resolveFn,
 		dataName: 'value'
 	};
-	enhanced[REJECTED] = {
+	enhancedState[REJECTED] = {
 		state: REJECTED,
 		otherState: FULFILLED,
 		isStateFn: isRejected,
@@ -53,143 +194,38 @@ module.exports = (function(global) {
 		dataName: 'reason'
 	}
 
-	var cache = (function() {
+	function enhancePromise(promise, resolve, reject) {
 
-		var map = {};
-
-		function registerPromise(promise, resolve, reject) {
-			// register the promise into a map
-			// the key is the promise itself
-			map[promise] = {
-				// promise itself whose state can change
-				promise: promise,
-				// resolve function
-				resolve: resolve,
-				// reject function
-				reject: reject,
-				// then stack { onFulfillment, nextPromise returned by then }
-				onfulfilled : [],
-				// then stack { onRejection, nextPromise returned by then }
-				onrejected: []
-			};
-		}
-
-		function registerThen(promise, nextPromise, onFulfillment, onRejection) {
-
-			promise.a && console.log(promise.ID, map[promise].promise);
-
-			// update the promise property in the map - its state may have 
-			// changed
-			map[promise].promise = promise;
-
-			// register the onFulfillment callback in its stack, with the 
-			// nextPromise by the then function
-			map[promise].onfulfilled.push({
-				nextPromise: nextPromise,
-				callback: onFulfillment
-			});
-
-			// register the onRejection callback in its stack, with the 
-			// nextPromise by the then function
-			map[promise].onrejected.push({
-				nextPromise: nextPromise,
-				callback: onRejection
-			});
-
-			// if the promise is already fulfilled
-			if (isFulfilled(promise)) {
-				// execute all the onFulfilled callbacks register within the
-				// various then calls
-				clean(promise, enhanced[FULFILLED]);
-
-			} else if (isRejected(promise)) {
-				// execute all the onRejected callbacks register within the
-				// various then calls				
-				clean(promise, enhanced[REJECTED]);
-
-			}
-		}		
-
-		function clean(promise, o) {
-
-			// if the promise is not pending
-			// then it's either resolved or rejected
-			if (o.isStateFn(promise)) {
-				// the registered object mapped to the promise
-				var registeredPromise = map[promise];
-				// the then stacked based on the promise state
-				var stack = registeredPromise['on' + o.state];
-
-				// while there are elements in the stack
-				while (stack.length) {
-
-					// 2.2.2: If `onFulfilled` is a function
-					// 2.2.2.3: it must not be called more than once.
-					// remove the first element of the stack
-					// then calls are handled on registering order
-					var element = stack.shift();
-
-					// the nextPromise returned by the specific then
-					var nextPromise = element.nextPromise;
-					// the then callback (onFulfillment or onRejection)
-					var callback = element.callback;
-					// the data of the promise (value or reason)
-					var data = promise[o.dataName];
-
-					// 2.2.1: Both `onFulfilled` and `onRejected` are optional 
-					// arguments.
-					// 2.2.1.1: If `onFulfilled` is not a function, it must be 
-					// ignored.
-					// 2.2.1.2: If `onRejected` is not a function, it must be 
-					// ignored.
-
-					if (isFn(callback)) {
-
-						// 2.2.4: `onFulfilled` or `onRejected` must not be 
-						// called until the execution context stack contains 
-						// only platform code.
-						setTimeout(function() {
-
-							// 2.2.2: If `onFulfilled` is a function
-							// 2.2.2.1: it must be called after `promise` is 
-							// fulfilled, with `promise`’s fulfillment value 
-							// as its first argument.
-            				// 2.2.2.2: it must not be called before `promise` 
-            				// is fulfilled
-							// 2.2.3: If `onRejected` is a function,
-							// 2.2.3.1: it must be called after `promise` is 
-							// rejected, with `promise`’s rejection reason as 
-							// its first argument.
-            				// 2.2.3.2: it must not be called before `promise` 
-            				// is rejected
-            				// 2.2.3.3: it must not be called more than once.
-							// be careful here, callback can be asynchronous
-
-							// 2.2.5 `onFulfilled` and `onRejected` must be 
-							// called as functions (i.e. with no `this` value).
-							// -> thus the apply with undefined as context
-							callback.apply(undefined, [ data ]);
-						}, 0);
- 
-					} else {
-						// resolve / reject the nextPromise
-						// with the value / reason of the invoking promise
-						o.execFn(nextPromise)(data);
-					}
-				}
-				// clear the other stack
-				registeredPromise['on' + o.otherState] = [];
-			}
-		}	
-				
 		return {
-			clean: clean,
-			registerPromise: registerPromise,
-			registerThen: registerThen,
-			map: map
+			// promise itself whose state can change
+			promise: promise,
+			// resolve function
+			resolve: resolve,
+			// reject function
+			reject: reject,
+			// then stack { onFulfillment, nextEnhancedPromise returned by then }
+			onfulfilled : [],
+			// then stack { onRejection, nextEnhancedPromise returned by then }
+			onrejected: []
 		};
 
-	})();
+	}
+
+	function then(enhancedPromise, onFulfillment, onRejection) {
+
+		var nextPromise = new Promise(emptyFn);
+		var nextEnhancedPromise = enhancePromise(nextPromise);
+
+		// 2.2.1: Both `onFulfilled` and `onRejected` are optional arguments.
+		// 2.2.1.1: If `onFulfilled` is not a function, it must be ignored.
+		// 2.2.1.2: If `onRejected` is not a function, it must be ignored.
+		registerThen(enhancedPromise, nextEnhancedPromise, onFulfillment, onRejection);
+
+		// 2.2.7: `then` must return a promise: 
+		// `promise2 = promise1.then(onFulfilled, onRejected)`
+		return nextPromise;
+
+	}
 
 	function Promise(executor) {
 
@@ -201,12 +237,17 @@ module.exports = (function(global) {
 
 		this.state = PENDING;
 
-		this.ID = ++id;
+		var resolve = undefined;
+		var reject = undefined;
+		var promise = this;
+		var enhancedPromise = enhancePromise(this, resolve, reject);
 
-		var resolve = resolveFn(this);
-		var reject = rejectFn(this);
+		resolve = resolveFn(enhancedPromise);
+		reject = rejectFn(enhancedPromise);
 
-		cache.registerPromise(this, resolve, reject);
+		this.then = function(onFulfillment, onRejection) {
+			return then(enhancedPromise, onFulfillment, onRejection);
+		};
 
 		executor.apply(undefined, [ resolve, reject ]);
 
@@ -214,30 +255,16 @@ module.exports = (function(global) {
 
 	function resolve(value) {
 		var promise = new Promise(emptyFn);
-		resolveFn(promise)(value);
+		promise.state = FULFILLED;
+		promise.value = value;
 		return promise;
 	}
 
 	function reject(reason) {
 		var promise = new Promise(emptyFn);
-		rejectFn(promise)(reason);
+		promise.state = REJECTED;
+		promise.reason = reason;
 		return promise;
-	}
-
-	function then(onFulfillment, onRejection) {
-
-		var promise = this;
-		var nextPromise = new Promise(emptyFn);
-
-		// 2.2.1: Both `onFulfilled` and `onRejected` are optional arguments.
-		// 2.2.1.1: If `onFulfilled` is not a function, it must be ignored.
-		// 2.2.1.2: If `onRejected` is not a function, it must be ignored.
-		cache.registerThen(promise, nextPromise, onFulfillment, onRejection);
-
-		// 2.2.7: `then` must return a promise: 
-		// `promise2 = promise1.then(onFulfilled, onRejected)`
-		return nextPromise;
-
 	}
 
 	Promise.resolve = resolve;
